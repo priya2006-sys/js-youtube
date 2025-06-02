7b10bef47b02401f4d30a5b49ab3b3ddb7199671
Priya Tiwari <justpriya2007@gmail.com>
2025-06-02 15:54:24 +0000

    conversion done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "22abc"
+
+//  console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "22"=> 22
+// "22abc"=> NaN
+//true => 1 ; false =>0 
+
+let isLoggedIn = "Priya"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// ""=> false; "priya"=>true
+
+let someABC = 33
+let stringABC = String(someABC);
+console.log(stringABC);
+console.log(typeof stringABC);
+
