f9cf52dcdf5771ffb97b5a547e4cd3e500d396da
Priya Tiwari <justpriya2007@gmail.com>
2025-06-02 15:27:58 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use scrict" // treat all the code as a newer version
+
+//alert(3+3) // we are using node not browser
+// code readibity is must
+
+let name = "Priya";
+let age = 18;
+let isLoggedIn = true
+
+// number => 2 to power 53
+// string =>""
+// bigInt
+// boolean => true/false
+// undefined
+// symbol=> unique
+// object
+
+console.log(typeof undefined);
