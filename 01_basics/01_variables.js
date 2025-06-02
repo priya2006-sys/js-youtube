104854b6c8c02755a80175d766e59c6443ae80d2
Priya Tiwari <justpriya2007@gmail.com>
2025-06-02 14:52:12 +0000

    varible done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 12345
+let accountEmail ="priya@tiwari20gmail.com"
+var accountPassword ="2222"
+accountCity ="Mumbai"
+ let accountState;
+
+//   accountID = 54321 // cant change
+  accountEmail = "priya@t20gmail.com" 
+  accountPassword = "1111"
+  accountCity = "Lucknow"
+ console.log(accountCity);
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
