b51211f2e5062c7fd9cee4685ed7dc351f38b0cc
Priya Tiwari <justpriya2007@gmail.com>
2025-06-03 08:19:04 +0000

    datatypescomparison done

---
+++ 01_basics/04_datatypescomparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2"<1);
+// console.log("2">1);
+
+// ((((
+// (console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null<=0);
+// console.log(null>=0);
+
+// console.log(undefined<0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined==0);
+// ))))))in most of times we avoid these types of comparison
+
+// [===] => strict check(also check datatypes)
+
+console.log("2"==2);
+console.log("2"===2);
