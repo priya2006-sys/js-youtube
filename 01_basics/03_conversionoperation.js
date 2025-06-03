1caf2bc4e1b8784e1bc5d7962a86be2e9c7e9e5f
Priya Tiwari <justpriya2007@gmail.com>
2025-06-03 07:30:12 +0000

    conversionoperation done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -19,6 +19,33 @@ let isLoggedIn = "Priya"
 
 let someABC = 33
 let stringABC = String(someABC);
-console.log(stringABC);
-console.log(typeof stringABC);
+// console.log(stringABC);
+// console.log(typeof stringABC);
+
+// ****************************Operations*************************************************
+let value = 3;
+let NegValue = -value;
+// console.log(NegValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);
+
+let str1 = "Priya";
+let str2 = "Tiwari";
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1"+ 2 + 2 );
+// console.log(1+ 2 + "2");
+
+
+
+
+
+
 
