84d1af8b535d0d69b61ad6b90199e76b33663197
Priya Tiwari <justpriya2007@gmail.com>
2025-06-03 15:54:13 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+const name = "Priya"
+const repoCount = 50
+
+// console.log(name +repoCount+ "value" );
+//  console.log(`Hello my name is ${name} and my repocount is ${repoCount}.` ); 
+// preferred to use this(``) method instead of traditional method
+
+ const gameName = new String("Priya-abc-pqr")
+//  console.log(gameName[0]);
+//  console.log(gameName.__proto__);
+
+//  console.log(gameName.length);
+//  console.log(gameName.toLowerCase());
+//  console.log(gameName.charAt(1));
+//  console.log(gameName.indexOf('P'));
+
+ const newString =gameName.substring(0, 4)
+//  console.log(newString);
+
+  const anotherString =gameName.slice(-8, 4)
+//   console.log(anotherString);
+
+  const newStringOne = "        Priya      "
+//   console.log(newStringOne);
+//   console.log(newStringOne.trim());
+
+  const collName = "priya12345tiwari"
+//   console.log(collName.replace('12345', '2345'));
+//   console.log(collName.includes('shriya'));
+  console.log(gameName.split('-'));
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
