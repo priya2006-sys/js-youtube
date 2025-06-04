67a0c700c0779de785e8d8af1edff1a3c23bc8ba
Priya Tiwari <justpriya2007@gmail.com>
2025-06-04 13:26:05 +0000

    arrays done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+//array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const heroes =["Marvel", "Naagraj", "Shaktimaan"] 
+
+// another way to declare array
+const myArr = new Array(1, 2, 3, 4)
+
+// console.log(heroes[0]);
+
+//Array methods
+
+// myArray.push(7)
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(7)
+// myArray.shift(7)
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(newArr);
+
+// slice, splice
+
+// console.log("A", myArray);
+
+const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B", myArray);
+
+const myn2 = myArr.splice(1, 3)
+// console.log(myn1);
+// console.log(myn2);
+
+
+
+
+
+
