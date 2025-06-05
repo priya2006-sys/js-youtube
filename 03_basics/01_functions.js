43669879debd8cb5bc0227825b4d59dc765c472f
Priya Tiwari <justpriya2007@gmail.com>
2025-06-05 08:36:06 +0000

    functions 2 done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,13 @@
-function sayMyName(){
-    console.log("P");
-     console.log("R");
-      console.log("I");
-       console.log("Y");
-        console.log("A");
-} // function definition
+// // function sayMyName(){
+//     console.log("P");
+//      console.log("R");
+//       console.log("I");
+//        console.log("Y");
+//         console.log("A");
+// }
+ // function definition
 
-sayMyName()//  when we put parenthesis it is called as function execution 
+// sayMyName()//  when we put parenthesis it is called as function execution 
 
 // function addTwoNumbers(num1, num2)//parameters
 // {
@@ -26,12 +27,46 @@ return num1 + num2
 
 function LoginUserMessage(username ="Sam"){
     if(username === undefined){
-console.log("Please enter the user name");
+// console.log("Please enter the user name");
 return
     }
 return `${username} just logged in  `
 }
  
 // console.log(LoginUserMessage("Priya"));
- console.log(LoginUserMessage());
+//  console.log(LoginUserMessage());
 
+
+
+
+// function calculateCardPrice (...num1){
+// return num1
+// }
+function calculateCardPrice (val1, val2, ...num1){
+return num1
+}
+
+
+// console.log(calculateCardPrice(200, 400, 500));
+
+const user ={
+    username: "Priya",
+    price: 200
+}
+
+function handleObject(anyobject){
+// console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+
+handleObject({
+    username: "Priya",
+    price: 300
+})
+
+const myNewArray = [200, 400, 600, 100]
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+console.log(returnSecondValue(myNewArray));
