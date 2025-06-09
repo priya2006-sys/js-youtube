dadc6939d43f480e0b86b3cd9d4789e6213f74ce
Priya Tiwari <justpriya2007@gmail.com>
2025-06-09 08:09:37 +0000

    while loop

---
+++ 05_iterations/02.js
@@ -0,0 +1,18 @@
+let index = 0
+// while (index <= 10) {
+//  console.log(`value of index is ${index}`);
+//     index = index +2
+// }
+
+let myArray = ["Priya" , "Shriyansh", "Rishabh", "Prarthana"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr +1
+   
+}
+
+do{
+console
+}while (condition);
