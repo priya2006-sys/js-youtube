a15d7c9ec411c9404115a7229f0922e991177ee9
Priya Tiwari <justpriya2007@gmail.com>
2025-06-09 07:43:10 +0000

    for loop

---
+++ 05_iterations/01.js
@@ -0,0 +1,46 @@
+//for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i; 
+    if(element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+// console.log(`outer loop ${i}`);
+    for (let j = 0; j <=  10; j++) {
+    //   console.log(`inner loop ${j}`);
+        // console.log(i + '*' + j + '=' + i*j);
+          
+    }
+}
+
+let myArray = ["Priya" , "Shriyansh", "Rishabh", "Prarthana"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+     
+}
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if ( i == 5){
+//         console.log("detected");
+//         break;
+//     }
+//   console.log(`value of I is ${i}`);
+  
+// }
+
+for (let i = 1; i <= 20; i++) {
+    if ( i == 5){
+        console.log("detected");
+        continue;
+    }
+  console.log(`value of I is ${i}`);
+  
+}
+
