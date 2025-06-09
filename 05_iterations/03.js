64f85c541c802562520e18db7fcc6a33de658a2c
Priya Tiwari <justpriya2007@gmail.com>
2025-06-09 13:58:37 +0000

    iterations

---
+++ 05_iterations/03.js
@@ -0,0 +1,34 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+  
+    
+    
+}
+
+const greetings = "hello world"
+for (const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+}
+
+// maps (for unique values)
+
+const map = new Map()
+map.set()
+
+for (const element of object) {
+    
+}
+
+const myObject ={
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+// for (const [key, value] of object) {
+//     console.log(key, ':-', value);
+    
+// }
+
---
+++ 05_iterations/05.js
@@ -0,0 +1,41 @@
+const coding = ["js", "cpp", "C", "Java", "py"]
+
+coding.forEach( function(val) {
+    // console.log(val);
+    
+})
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// }  )
+
+// function printMe (item){
+//     console.log(item);
+    
+// }
+//  coding.forEach (printMe)
+
+// coding.forEach( (item, index, arr) =>  {
+// console.log(item, index, arr);
+
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFilename: "js"
+    },
+    {
+        languageName: "java",
+        languageFilename: "java"
+    },
+    {
+        languageName: "python",
+        languageFilename: "py"
+    },
+]
+myCoding.forEach( (item) => {
+    console.log(item.languageFilename);
+    
+})
